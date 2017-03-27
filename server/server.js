6eed0a01fbf9cd4de4d0cddd6c067b17dac6c52b
Anh Vu <anhvu869492@gmail.com>
2017-03-27 23:30:58 +0700

    Great new user , and alert other

--- server/server.js
+++ server/server.js
@@ -13,17 +13,28 @@ io.on('connection', (socket) =>{
   console.log('New user connected');
 
   socket.emit('newMessage',{
-    from: 'Vu',
-    text: 'VuDz',
+    from: 'Admin',
+    text: 'Welcome to chat App',
     createAt: 123123
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user joined',
+    createAt: new Date().getTime()
   });
 
   socket.on('createMessage' , (message) =>{
     console.log('Create message ',message);
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createAt: new Date().getTime()
+    // io.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        createAt: new Date().getTime()
     });
 
   });
